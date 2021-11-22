58f4c3f7ffcaa75e59bb8f25efa2bb4ebfeae176
vikram14 <43388339+vikram14@users.noreply.github.com>
2021-11-21 22:34:44 -0500

    LandingPage added

---
+++ socket-chess/src/components/landing.js
@@ -0,0 +1,49 @@
+import {Component} from "react";
+//import Login './Login';
+
+class LandingPage extends Component {
+
+    constructor(props){
+      super(props);
+      this.state={'logIn':false, 'register':false};
+    }
+
+    handleLogIn(){
+        this.setState({'logIn':true, 'register':false});
+    }
+
+    handleRegister(){
+        this.setState({'logIn':false, 'register':true});
+    }
+
+    handleHome(){
+        this.setState({'logIn':false, 'register':false});
+    }
+
+    render(){
+        if(this.state.logIn===true && this.state.register===false){
+            return (<div>
+                <p>Add Login Frame here</p>
+                <button  onClick={this.handleRegister.bind(this)}>Register</button>
+                <button onClick={this.handleHome.bind(this)}>Home</button>
+            </div>);
+        }
+        else if(this.state.logIn===false && this.state.register===true){
+            return (<div>
+                <p>Add Register Frame here</p>
+                <button  onClick={this.handleLogIn.bind(this)}>Login</button>
+                <button onClick={this.handleHome.bind(this)}>Home</button>
+            </div>);
+        }
+        else{
+            return (
+                <div>
+                    <button  onClick={this.handleLogIn.bind(this)}>Login</button>
+                    <button  onClick={this.handleRegister.bind(this)}>Register</button>
+                </div>
+            );
+        }
+    }
+}
+
+export default LandingPage;
