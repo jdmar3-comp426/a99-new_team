aaf40c81ea5696bd23056a8ce8e827427cb3db54
MaddisonK <maddk@live.unc.edu>
2021-11-22 23:10:12 -0500

    basic auth

--- socket-chess/src/authentication/authenticator.js
+++ socket-chess/src/authentication/authenticator.js
@@ -1,9 +1,32 @@
+import axios from 'axios';
+
 class Authenticator{
-    LogIn(){}
+
+    constructor() {
+        this.authenticated = False;
+    }
+    LogIn(username, password)
+    //userPass is an object with username and password
+    {
+
+        axios.post('/user', {
+            user: username,
+            pass: password
+          })
+          .then(function (response) {
+            console.log(response);
+            this.authenticated = True;
+          })
+          .catch(function (error) {
+            console.log(error);
+          });
+    }
     LogOut(){}
-    isAuthenticated(){}
+    isAuthenticated(){
+        return this.authenticated;
+    }
 }
 
-const auth= new Authenticator();
+const auth = new Authenticator();
 
 export default auth;
